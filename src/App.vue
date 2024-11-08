aca5408afaf9c66879a64473beb32320062b0680
zsf <1279664295@qq.com>
2024-11-08 15:57:50 +0800

    完成弹窗

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,8 @@
     </div>
 
     <div class="query">
-      <el-input v-model="input" style="width: 240px" placeholder="Please input" />
-      <el-button type="primary">增加</el-button>
+      <el-input v-model="input" style="width: 240px" placeholder="请输入姓名" />
+      <el-button type="primary" @click="handleAdd">增加</el-button>
     </div>
 
     <div class="table">
@@ -16,23 +16,56 @@
        :data="tableData"
         style="width: 100%"
         @selection-change="handleSelectionChange" border>
-      <el-table-column type="selection"  width="55" />
-      <el-table-column fixed prop="date" label="Date" width="150" />
-      <el-table-column prop="name" label="Name" width="120" />
-      <el-table-column prop="state" label="State" width="120" />
-      <el-table-column prop="city" label="City" width="120" />
-      <el-table-column prop="address" label="Address" width="600" />
-      <el-table-column prop="zip" label="Zip" width="120" />
-      <el-table-column fixed="right" label="Operations" min-width="120">
+      <el-table-column type="selection"  width="40" />
+      <el-table-column fixed prop="name" label="姓名" width="150" />
+      <el-table-column prop="phone" label="电话" width="120" />
+      <el-table-column prop="email" label="邮箱" width="120" />
+      <el-table-column prop="state" label="状态" width="120" />
+      <el-table-column prop="address" label="地址" width="120" />
+      <el-table-column fixed="right" label="操作" min-width="120">
         <template #default>
           <el-button link type="primary" size="small" @click="handleClick">
-            Detail
+            删除
           </el-button>
-          <el-button link type="primary" size="small">Edit</el-button>
+          <el-button link type="primary" size="small">
+            修改
+          </el-button>
         </template>
       </el-table-column>
     </el-table>
     </div>
+  </div>
+
+  <div class="dialog">
+    <el-dialog v-model="dialogFormVisible" title="Shipping address" width="500">
+    <el-form :model="dialogForm">
+      <el-form-item label="姓名" :label-width="100">
+        <el-input v-model="dialogForm.name" autocomplete="off" />
+      </el-form-item>
+      <el-form-item label="电话" :label-width="100">
+        <el-input v-model="dialogForm.phone" autocomplete="off" />
+      </el-form-item>
+      <el-form-item label="邮箱" :label-width="100">
+        <el-input v-model="dialogForm.email" autocomplete="off" />
+      </el-form-item>
+      <el-form-item label="状态" :label-width="100">
+        <el-input v-model="dialogForm.state" autocomplete="off" />
+      </el-form-item>
+      <el-form-item label="地址" :label-width="100">
+        <el-input v-model="dialogForm.address" autocomplete="off" />
+      </el-form-item>
+    </el-form>
+    <template #footer>
+      <div class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">
+          取消
+        </el-button>
+        <el-button type="primary" @click="dialogFormVisible = false">
+          确认
+        </el-button>
+      </div>
+    </template>
+  </el-dialog>
   </div>
 </template>
 
@@ -41,45 +74,23 @@
   import { ref } from 'vue';
   //数据
   let tableData = ref([{
-    date: '2016-05-03',
-    name: 'Tom',
-    state: 'California',
-    city: 'Los Angeles',
-    address: 'No. 189, Grove St, Los Angeles',
-    zip: 'CA 90036',
-    tag: 'Home',
-  },
-  {
-    date: '2016-05-02',
-    name: 'Tom',
-    state: 'California',
-    city: 'Los Angeles',
-    address: 'No. 189, Grove St, Los Angeles',
-    zip: 'CA 90036',
-    tag: 'Office',
-  },
-  {
-    date: '2016-05-04',
-    name: 'Tom',
-    state: 'California',
-    city: 'Los Angeles',
-    address: 'No. 189, Grove St, Los Angeles',
-    zip: 'CA 90036',
-    tag: 'Home',
-  },
-  {
-    date: '2016-05-01',
-    name: 'Tom',
-    state: 'California',
-    city: 'Los Angeles',
-    address: 'No. 189, Grove St, Los Angeles',
-    zip: 'CA 90036',
-    tag: 'Office',
-  },])
+    name: '张三',
+    phone: '10086',
+    email: '123@qq.com',
+    state: '离线',
+    address: '地球',
+  }])
+  let dialogForm = ref({
+    name: "",
+    phone: "",
+    email: "",
+    state: "",
+    address: "",
+  })
 
   let input = ref("");
   let multipleSelection = ref([]);
-
+  let dialogFormVisible = ref(false);
   //函数
   const handleClick = () => {
     console.log('click')
@@ -89,7 +100,9 @@ const handleSelectionChange = (val:[]) => {
   multipleSelection.value = val
   console.log(val)
 }
-
+const handleAdd = ()=>{
+  dialogFormVisible.value = true;
+}
 </script>
 
 
@@ -99,17 +112,14 @@ const handleSelectionChange = (val:[]) => {
 
 .table-box{
   width: 800px;
-  left: 50%;
-  top:30%;
-  position: absolute;
-  transform: translate(-50%,-50%);
+  margin: 200px;
 }
 .title{
   display: flex;
   justify-content: center;
 }
 .query{
-  padding:0px 40px 0px 0px;
+  padding:0px 40px 10px 0px;
   margin: auto;
   display: flex;
   justify-content:space-between;
