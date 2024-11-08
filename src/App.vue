7c6597ff6f2c4a7d96fe5b78107a88a9790d097e
zsf <1279664295@qq.com>
2024-11-08 20:03:36 +0800

    完成前端

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,12 @@
     </div>
 
     <div class="query">
-      <el-input v-model="input" style="width: 240px" placeholder="请输入姓名" />
-      <el-button type="primary" @click="handleAdd">增加</el-button>
+      <el-input v-model="input" style="width: 240px" placeholder="请输入姓名" @input="search"/>
+      <span>
+        <el-button type="primary" @click="handleAdd">增加</el-button>
+        <el-button type="danger" @click="handleDelList" v-show="multipleSelection.length>0">删除</el-button>
+      </span>
+      
     </div>
 
     <div class="table">
@@ -21,14 +25,14 @@
       <el-table-column prop="phone" label="电话" width="120" />
       <el-table-column prop="email" label="邮箱" width="120" />
       <el-table-column prop="state" label="状态" width="120" />
-      <el-table-column prop="address" label="地址" width="120" />
+      <el-table-column prop="address" label="地址" width="300" />
       <el-table-column fixed="right" label="操作" min-width="120">
-        <template #default>
-          <el-button link type="primary" size="small" @click="handleClick">
+        <template #default="scope">
+          <el-button link type="primary" size="small" @click="handleRowDel(scope.row)" style="color:#F56C6C">
             删除
           </el-button>
-          <el-button link type="primary" size="small">
-            修改
+          <el-button link type="primary" size="small" style="color:#409EFF" @click="handleEdit(scope.row)">
+            编辑
           </el-button>
         </template>
       </el-table-column>
@@ -37,7 +41,7 @@
   </div>
 
   <div class="dialog">
-    <el-dialog v-model="dialogFormVisible" title="Shipping address" width="500">
+    <el-dialog v-model="dialogFormVisible" :title="dialogTitle==='add'?'增加':'编辑'" width="500">
     <el-form :model="dialogForm">
       <el-form-item label="姓名" :label-width="100">
         <el-input v-model="dialogForm.name" autocomplete="off" />
@@ -60,7 +64,7 @@
         <el-button @click="dialogFormVisible = false">
           取消
         </el-button>
-        <el-button type="primary" @click="dialogFormVisible = false">
+        <el-button type="primary" @click="dialogconfirm">
           确认
         </el-button>
       </div>
@@ -74,14 +78,30 @@
   import { ref } from 'vue';
   //数据
   let tableData = ref([{
+    id:"1",
     name: '张三',
+    phone: '10086',
+    email: '123@qq.com',
+    state: '离线',
+    address: '地球',
+  },{
+    id:"2",
+    name: '李三',
+    phone: '10086',
+    email: '123@qq.com',
+    state: '离线',
+    address: '地球',
+  },{
+    id:"3",
+    name: '王三',
     phone: '10086',
     email: '123@qq.com',
     state: '离线',
     address: '地球',
   }])
   let dialogForm = ref({
-    name: "",
+    id:"",
+    name: "aa",
     phone: "",
     email: "",
     state: "",
@@ -89,20 +109,131 @@
   })
 
   let input = ref("");
-  let multipleSelection = ref([]);
+  let multipleSelection =ref<Object[]>([]);
   let dialogFormVisible = ref(false);
-  //函数
-  const handleClick = () => {
-    console.log('click')
-}
+  let dialogTitle = ref("");
+  let dialogState = ref(true);
+  let editIndex = ref("");
 
-const handleSelectionChange = (val:[]) => {
-  multipleSelection.value = val
-  console.log(val)
-}
+  let tableDataCopy = ref([{
+    id:"1",
+    name: '张三',
+    phone: '10086',
+    email: '123@qq.com',
+    state: '离线',
+    address: '地球',
+  },{
+    id:"2",
+    name: '李三',
+    phone: '10086',
+    email: '123@qq.com',
+    state: '离线',
+    address: '地球',
+  },{
+    id:"3",
+    name: '王三',
+    phone: '10086',
+    email: '123@qq.com',
+    state: '离线',
+    address: '地球',
+  }])
+
+
+  //函数
+ 
+
+
+  //增加数据
 const handleAdd = ()=>{
   dialogFormVisible.value = true;
+  dialogState.value = true;
+  dialogTitle.value = "add";
+  dialogForm.value = {
+    id:"",
+    name: "",
+    phone: "",
+    email: "",
+    state: "",
+    address: "",
+  }
 }
+
+const dialogconfirm = ()=>{
+  dialogFormVisible.value = false;
+  if(dialogState.value){
+     //拿数据
+  //添加到数组中
+  tableData.value.push({
+    id: (tableData.value.length+1).toString(),
+    name: dialogForm.value.name,
+    phone: dialogForm.value.phone,
+    email: dialogForm.value.email,
+    state: dialogForm.value.state,
+    address: dialogForm.value.address,
+  })
+  }else{
+    let index = tableData.value.findIndex(item=>item.id===dialogForm.value.id)
+    tableData.value[index] = dialogForm.value;
+  }
+  tableDataCopy.value = tableData.value; 
+  console.log(tableDataCopy);
+}
+
+
+//删除数据
+const handleRowDel=({id}:any) => {
+    console.log(id)
+
+    let index = tableData.value.findIndex(item=>item.id===id)
+    tableData.value.splice(index,1)
+    tableDataCopy.value = tableData.value; 
+}
+
+//选中
+const handleSelectionChange = (val: any[]) => {
+multipleSelection.value = [];
+val.forEach((item: { id: any; }) =>{
+  multipleSelection.value.push(item.id)
+})
+
+}
+//多删
+const handleDelList =()=>{
+  multipleSelection.value.forEach(id=>{
+    handleRowDel(id);
+  })
+  multipleSelection.value = [];
+}
+
+//改
+const handleEdit = (row:any)=>{
+  dialogTitle.value = "edit";
+  dialogState.value = false;
+  dialogFormVisible.value = true;
+
+  dialogForm.value = {
+    id:`${row.id}`,
+    name: `${row.name}`,
+    phone: `${row.phone}`,
+    email: `${row.email}`,
+    state: `${row.state}`,
+    address: `${row.address}`,
+  }
+  editIndex.value = row.id;
+}
+
+//查
+const search = (val:any)=>{
+  tableData.value = tableDataCopy.value
+  if(val.length>0){
+    tableData.value = tableData.value.filter(item=>item.name.match(val));
+
+  }else{
+    tableData.value = tableDataCopy.value;
+    
+  }
+}
+
 </script>
 
 
@@ -119,7 +250,7 @@ const handleAdd = ()=>{
   justify-content: center;
 }
 .query{
-  padding:0px 40px 10px 0px;
+  padding:0px 0px 10px 0px;
   margin: auto;
   display: flex;
   justify-content:space-between;
